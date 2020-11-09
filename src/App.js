b27193d408ceb8e582028504829ff52e353df32d
kennycha <dudqn136@naver.com>
2020-11-09 14:06:46 +0900

    Add file input and file-loading logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import styled from 'styled-components';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+const Input = styled.input`
+  display: block;
+`
 
 function App() {
+  const [loadedAnimations, setLoadedAnimations] = useState(null)
+
+
+  // File 업로드 시 읽어서 animations 뽑아내서 컴포넌트 상태(loadedAnimations) 변경
+  const onFileChange = (event) => {
+    const file = event.target.files[0]
+    if (file) {
+      const fileURL = URL.createObjectURL(file)
+      const loader = new GLTFLoader() // loader 생성
+      loader.load(fileURL, gltf => {
+        setLoadedAnimations(gltf.animations)
+        console.log('gltf.animations: ', gltf.animations)
+      })
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Input type='file' accept='.glb' onChange={onFileChange} />
+      <div>{loadedAnimations?.length}</div>
+    </>
   );
 }
 
